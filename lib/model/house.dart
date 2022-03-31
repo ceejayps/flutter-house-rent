182001c3536c1c0e7e3620c591d49adc9a3ed40f
ceejayps <ceejayps1308@gmail.com>
2022-03-31 14:10:49 -0500

    Update house.dart

--- lib/model/house.dart
+++ lib/model/house.dart
@@ -7,18 +7,18 @@ class House {
 
   static List<House> generateRecommended() {
     return [
-      House('The Moon House', 'P455, Chhatak, Sylhet',
-          'assets/images/house01.jpeg'),
-      House('The Moon House', 'P455, Chhatak, Sylhet',
+      House(
+          'Water Ford', 'P455, Chhatak, Sylhet', 'assets/images/house01.jpeg'),
+      House('The Moon party', 'P455, Chhatak, Sylhet',
           'assets/images/house02.jpeg'),
     ];
   }
 
   static List<House> generateBestOffer() {
     return [
-      House('The Moon House', 'P455, Chhatak, Sylhet',
-          'assets/images/offer01.jpeg'),
-      House('The Moon House', 'P455, Chhatak, Sylhet',
+      House(
+          'Mad Max 3e', 'P455, Chhatak, Sylhet', 'assets/images/offer01.jpeg'),
+      House('Duty Gyal Dash Wea', 'P455, Chhatak, Sylhet',
           'assets/images/offer02.jpeg'),
     ];
   }
