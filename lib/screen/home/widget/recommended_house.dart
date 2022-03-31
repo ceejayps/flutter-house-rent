1ee1a0c6b98646761f3f0927179e984d46f7c929
ceejayps <ceejayps1308@gmail.com>
2022-03-31 12:13:17 -0500

    Update recommended_house.dart

--- lib/screen/home/widget/recommended_house.dart
+++ lib/screen/home/widget/recommended_house.dart
@@ -49,8 +49,10 @@ class RecommendedHouse extends StatelessWidget {
                           right: 0,
                           child: Container(
                             decoration: BoxDecoration(
-                              color: Color.fromARGB(157, 46, 46, 46),
-                            ),
+                                color: Color.fromARGB(157, 46, 46, 46),
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(12),
+                                    bottomRight: Radius.circular(12))),
                             padding: EdgeInsets.all(10),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
